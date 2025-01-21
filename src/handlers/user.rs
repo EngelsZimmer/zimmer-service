02d8c68408494d7a1947bd390e966b0240856cf5
Tain <ktain030@gmail.com>
2025-01-21 17:25:57 +0900

    Add login and signup Unit Test

--- src/handlers/user.rs
+++ src/handlers/user.rs
@@ -23,4 +23,43 @@ pub async fn user_signup(
 }
 
 #[cfg(test)]
-mod test {}
+mod tests {
+    use super::*;
+    use actix_web::http::StatusCode;
+    use dotenv::dotenv;
+    use sqlx::postgres::PgPool;
+    use std::env;
+    #[actix_web::test]
+    async fn user_login_success() {
+        dotenv().ok();
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file.");
+        let db_pool = PgPool::connect(&database_url).await.unwrap();
+        let app_state = web::Data::new(AppState {
+            health_check_response: "".to_string(),
+            db: db_pool,
+        });
+        let user = web::Json(LoginUser {
+            user_email: "admin@example.com".to_string(),
+            user_password: "1234".to_string(),
+        });
+        let resp = user_login(app_state, user).await.unwrap();
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+    #[actix_web::test]
+    async fn user_signup_success() {
+        dotenv().ok();
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file.");
+        let db_pool = PgPool::connect(&database_url).await.unwrap();
+        let app_state = web::Data::new(AppState {
+            health_check_response: "".to_string(),
+            db: db_pool,
+        });
+        let user = web::Json(SignupUser {
+            user_email: "admin@example.com".to_string(),
+            user_password: "1234".to_string(),
+            user_name: "Admin".to_string(),
+        });
+        let resp = user_signup(app_state, user).await.unwrap();
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+}
